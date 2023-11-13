7d42ebd0bacbc9cd06c52c74773311ed8ca7cb92
Boris Rybalkin <ribalkin@gmail.com>
2023-11-13 13:52:31 +0000

    ld

--- app/collabora.sh
+++ app/collabora.sh
@@ -3,4 +3,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/usr/lib)
-${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/usr/bin/coolwsd "$@"
+${DIR}/lib/*-linux*/ld-*.so* --library-path $LIBS ${DIR}/usr/bin/coolwsd "$@"
